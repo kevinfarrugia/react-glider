2b52e5cae04e5642ac93e2266b91e116f99fd189
Kevin Farrugia <hello@imkev.dev>
2023-03-15 15:30:23 +0100

    Added dark-mode to demo page; Added anchors to headings

--- docs/demos.tsx
+++ docs/demos.tsx
@@ -20,61 +20,139 @@ function Demos() {
     <div className="demos">
       <h2>Demos</h2>
       <div className="item">
-        <h3>Multiple Items</h3>
+        <h3>
+          <a id="multiple_items" href="#multiple_items" className="anchor">
+            Multiple Items
+          </a>
+        </h3>
         <MultipleItemsGlider />
       </div>
       <div className="item">
-        <h3>Single Item with Scroll Lock</h3>
+        <h3>
+          <a
+            id="single_item_with_scroll_lock"
+            href="#single_item_with_scroll_lock"
+            className="anchor"
+          >
+            Single Item with Scroll Lock
+          </a>
+        </h3>
         <SingleItemGlider />
       </div>
       <div className="item">
-        <h3>Responsive Glider</h3>
+        <h3>
+          <a
+            id="responsive_glider"
+            href="#responsive_glider"
+            className="anchor"
+          >
+            Responsive Glider
+          </a>
+        </h3>
         <ResponsiveGlider />
       </div>
       <div className="item">
-        <h3>Autoplay Glider</h3>
+        <h3>
+          <a id="autoplay_glider" href="#autoplay_glider" className="anchor">
+            Autoplay Glider
+          </a>
+        </h3>
         <AutoplayGlider />
       </div>
       <div className="item">
-        <h3>Updating Props</h3>
+        <h3>
+          <a id="updating_props" href="#updating_props" className="anchor">
+            Updating Props
+          </a>
+        </h3>
         <UpdatingPropsGlider />
       </div>
       <div className="item">
         <h3>
-          <code>ref</code> exposes Glider methods
+          <a
+            id="ref_exposes_glider"
+            href="#ref_exposes_glider"
+            className="anchor"
+          >
+            <code>ref</code> exposes Glider methods
+          </a>
         </h3>
         <RefGlider />
       </div>
       <div className="item">
-        <h3>Perspective View</h3>
+        <h3>
+          <a id="perspective_view" href="#perspective_view" className="anchor">
+            Perspective View
+          </a>
+        </h3>
         <PerspectiveViewGlider />
       </div>
       <div className="item">
-        <h3>Scroll to slide #5</h3>
+        <h3>
+          <a id="scroll_to_slide" href="#scroll_to_slide" className="anchor">
+            Scroll to slide #5
+          </a>
+        </h3>
         <ScrollToGlider />
       </div>
       <div className="item">
-        <h3>Fractional Slides</h3>
+        <h3>
+          <a
+            id="fractional_slides"
+            href="#fractional_slides"
+            className="anchor"
+          >
+            Fractional Slides
+          </a>
+        </h3>
         <FractionalSlidesGlider />
       </div>
       <div className="item">
-        <h3>Custom Events</h3>
+        <h3>
+          <a id="custom_events" href="#custom_events" className="anchor">
+            Custom Events
+          </a>
+        </h3>
         <CustomEventsGlider />
       </div>
       <div className="item">
-        <h3>Custom Element Arrow</h3>
+        <h3>
+          <a
+            id="custom_element_arrow"
+            href="#custom_element_arrow"
+            className="anchor"
+          >
+            Custom Element Arrow
+          </a>
+        </h3>
         <CustomElementArrowsGlider />
       </div>
       <div className="item">
-        <h3>Unmounting / remounting</h3>
+        <h3>
+          <a
+            id="unmounting_remounting"
+            href="#unmounting_remounting"
+            className="anchor"
+          >
+            Unmounting / remounting
+          </a>
+        </h3>
         <RemountingGlider />
       </div>
       <div className="item">
-        <h3>Skip Track</h3>
+        <h3>
+          <a id="skip_track" href="#skip_track" className="anchor">
+            Skip Track
+          </a>
+        </h3>
         <SkipTrackGlider />
       </div>
       <div className="item">
-        <h3>Dynamic Data</h3>
+        <h3>
+          <a id="dynamic_data" href="#dynamic_data" className="anchor">
+            Dynamic Data
+          </a>
+        </h3>
         <DynamicDataGlider />
       </div>
     </div>
